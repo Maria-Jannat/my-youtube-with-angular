89cff46ad3ee17729ea3f18e34c9ba05e6c27763
Maria-Jannat <cesurmaria.007@gmail.com>
2020-12-02 12:19:50 +0600

    added youtube videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
-import { ContentHolderComponent } from './content-holder/content-holder.component';
+// import { ContentHolderComponent } from './content-holder/content-holder.component';
 import { HomeComponent } from './home/home.component';
 import { TrendingComponent } from './trending/trending.component';
 import { SubscriptionComponent } from './subscription/subscription.component';
 import { LibraryComponent } from './library/library.component';
 import { RouterModule } from '@angular/router';
+import { ContentHolderComponent } from './content-holder/content-holder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SideBarComponent,
-    ContentHolderComponent,
+    // ContentHolderComponent,
     HomeComponent,
     TrendingComponent,
     SubscriptionComponent,
@@ -31,10 +32,18 @@ import { RouterModule } from '@angular/router';
     MatIconModule,
     BrowserAnimationsModule,
     RouterModule.forRoot([
-      { path: "home", component: HomeComponent },
+      {
+        path: "home", component: HomeComponent
+        // children: [
+        //   {
+        //     path: 'content', // child route path
+        //     component: ContentHolderComponent, // child route component that the router renders
+        //   }]
+      },
       { path: "trending", component: TrendingComponent },
       { path: "subscription", component: SubscriptionComponent },
-      { path: "library", component: LibraryComponent }
+      { path: "library", component: LibraryComponent },
+      { path: "content", component: ContentHolderComponent }
     ])
   ],
   providers: [],
